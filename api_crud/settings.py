e7203f895b45a3e64686d481da1e7b00eefd6558
Jordan Noblejas <jordantx21@gmail.com>
2023-11-18 12:52:23 -0500

    settings

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,6 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/dist/static/')
+    os.path.join(BASE_DIR, 'frontend/dist/')
     # Puedes agregar mas carpetas si es necesario
 ]
